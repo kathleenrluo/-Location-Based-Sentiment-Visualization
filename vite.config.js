4c6f94cafe1c465f62f15cb2cc28ad2518f177a5
Kathleen Luo <krluo@usc.edu>
2026-01-04 12:14:48 -0800

    Update configuration for personal GitHub repository

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/project-team12/',
+  base: '/-Location-Based-Sentiment-Visualization/',
   build: {
     outDir: 'dist'
   }
